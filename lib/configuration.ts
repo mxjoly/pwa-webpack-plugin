a0c711c83215ce101e3e0fc8180decca68e2655d
Maxime Joly <maxime.joly2808@gmail.com>
2021-01-29 12:38:16 +0100

    Use ajv instead of json-schema

--- lib/configuration.ts
+++ lib/configuration.ts
@@ -1,30 +1,31 @@
 import safeRequire from 'safe-require';
-import jsonschema from 'json-schema';
 import chalk from 'chalk';
+import Ajv from 'ajv';
 
 const customIconsConfig = safeRequire('../../../../icons.json');
 const defaultIconsConfig = require('./icons.json');
+const schema = require('./schema.json');
+const ajv = new Ajv();
+const validate = ajv.compile(schema);
 
 export function getConfigurationFile() {
   if (customIconsConfig) {
-    const { valid } = jsonschema.validate(
-      customIconsConfig,
-      require('./schema.json')
-    );
+    const valid = validate(customIconsConfig);
 
     if (valid) {
       console.log(
         chalk.green(
-          'PWA Webpack Plugin: icons.json file has been detected to generate the icons.'
+          "PWA Webpack Plugin: './icons.json' is used to generate your icons."
         )
       );
       return customIconsConfig;
     } else {
       console.log(
         chalk.yellowBright(
-          'PWA Webpack Plugin: icons.json has an invalid json schema, the default configuration has been used instead. See https://github.com/mxjoly/pwa-webpack-plugin#configuration to get more informations.'
+          "PWA Webpack Plugin: './icons.json' has an invalid json schema, the default configuration has been used instead. See https://github.com/mxjoly/pwa-webpack-plugin#configuration to get more informations."
         )
       );
+      console.log(validate.errors);
       return defaultIconsConfig;
     }
   } else {
